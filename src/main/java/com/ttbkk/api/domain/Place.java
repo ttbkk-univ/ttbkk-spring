63f24ab2fd51f0fadd5a0f0d4673fa6bfa8c9da2
albegam <kookh92@gmail.com>
2022-06-27 05:23:15 +0900

    ORM Domain
    CHAR, VARCHAR, TEXT.-> String
    DATETIME -> Timestamp
    BIGINT -> BigInteger
    DECIMAL -> BigDecimal
    TINYINT -> boolean
    https://dev.mysql.com/doc/connector-j/5.1/en/connector-j-reference-type-conversions.html
    어노테이션은 잘 모르겠습니다.

---
+++ src/main/java/com/ttbkk/api/domain/Place.java
@@ -0,0 +1,143 @@
+package com.ttbkk.api.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+public class Place {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "latitude")
+    private BigDecimal latitude;
+    @Column(name = "longitude")
+    private BigDecimal longitude;
+    @Column(name = "is_deleted")
+    private boolean isDeleted;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+    @Column(name = "brand_id")
+    private String brandId;
+    @Column(name = "created_by_id")
+    private String createdById;
+    @Column(name = "updated_by_id")
+    private String updatedById;
+    @Column(name = "telephone")
+    private String telephone;
+    @Column(name = "address")
+    private String address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(BigDecimal latitude) {
+        this.latitude = latitude;
+    }
+
+    public BigDecimal getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(BigDecimal longitude) {
+        this.longitude = longitude;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(String brandId) {
+        this.brandId = brandId;
+    }
+
+    public String getCreatedById() {
+        return createdById;
+    }
+
+    public void setCreatedById(String createdById) {
+        this.createdById = createdById;
+    }
+
+    public String getUpdatedById() {
+        return updatedById;
+    }
+
+    public void setUpdatedById(String updatedById) {
+        this.updatedById = updatedById;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
