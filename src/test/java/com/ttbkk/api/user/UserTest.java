2357b13d4e01038a8228b246a17d5b08f40bbff0
JIWEON-JEONG <dlektl6@naver.com>
2022-07-25 19:59:42 +0900

    [DELETE] ORM 기능 테스트 삭제

--- src/test/java/com/ttbkk/api/user/UserTest.java
+++ src/test/java/com/ttbkk/api/user/UserTest.java
@@ -1,9 +1,6 @@
 
 package com.ttbkk.api.user;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -22,21 +19,6 @@ class UserTest {
      */
     @Autowired
     private EntityManager entityManager;
-
-    /**
-     * 엔티티 getCreatedAt null 검사 테스트.
-     */
-    @Test
-    @DisplayName("User 엔티티 getCreatedAt null 검사 테스트")
-    void userEntityVerify() {
-        User user = User.builder()
-                .socialId("test id")
-                .socialType("test type")
-                .build();
-
-        this.entityManager.persist(user);
-        Assertions.assertThat(user.getUpdatedAt()).isNotNull();
-    }
 
 }
 
