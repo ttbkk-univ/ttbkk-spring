e3ebd43ce7e8b9d5122324466205ff883c3ca9be
haze <skdml20022@gmail.com>
2022-08-13 14:30:16 +0900

    업데이트(수정) 관련 유저 id 컬럼 삭제

--- src/main/resources/db/migration/V5__add_review_table.sql
+++ src/main/resources/db/migration/V5__add_review_table.sql
@@ -5,10 +5,8 @@ CREATE TABLE review (
     created_at datetime(6) DEFAULT NULL,
     updated_at datetime(6) DEFAULT NULL,
     place_id char(32) NOT NULL,
-    created_by_id char(32) NOT NULL,
-    updated_by_id char(32) DEFAULT NULL,
+    user_id char(32) NOT NULL,
     PRIMARY KEY (id),
     CONSTRAINT review_place_id_fk_place_id FOREIGN KEY (place_id) REFERENCES place (id),
-    CONSTRAINT review_created_by_id_fk_user_id FOREIGN KEY (created_by_id) REFERENCES user (id),
-    CONSTRAINT review_updated_by_id_fk_user_id FOREIGN KEY (updated_by_id) REFERENCES user (id)
+    CONSTRAINT review_user_id_fk_user_id FOREIGN KEY (user_id) REFERENCES user (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
