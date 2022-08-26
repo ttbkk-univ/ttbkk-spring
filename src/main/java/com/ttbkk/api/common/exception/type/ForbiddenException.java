21f445f5cb95434ce928b027ec4617a862a37db0
JIWEON-JEONG <dlektl6@naver.com>
2022-08-26 20:52:04 +0900

    [FIX] parent class

--- src/main/java/com/ttbkk/api/common/exception/type/ForbiddenException.java
+++ src/main/java/com/ttbkk/api/common/exception/type/ForbiddenException.java
@@ -1,5 +1,6 @@
 package com.ttbkk.api.common.exception.type;
 
+import com.ttbkk.api.common.exception.BaseException;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
@@ -7,7 +8,7 @@ import org.springframework.http.HttpStatus;
  * HttpStatus 가 FORBIDDEN 인 예외들의 부모 클래스.
  */
 @Getter
-public class ForbiddenException extends RuntimeException {
+public class ForbiddenException extends BaseException {
 
     private final HttpStatus httpStatus = HttpStatus.FORBIDDEN;
 
