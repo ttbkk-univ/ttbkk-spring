f5345602ae72bc0f755a5b27b9433a951327d4e0
jeonghyun.an <jeonghyun.an@devsisters.com>
2022-09-03 17:42:47 +0900

    authorization 추가

--- src/main/java/com/ttbkk/api/config/SwaggerConfig.java
+++ src/main/java/com/ttbkk/api/config/SwaggerConfig.java
@@ -6,9 +6,17 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -17,6 +25,16 @@ public class SwaggerConfig {
     @Bean
     public Docket restAPI() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .securityContexts(
+                        Collections.singletonList(
+                                this.securityContext()
+                        )
+                )
+                .securitySchemes(
+                        Collections.singletonList(
+                                this.token()
+                        )
+                )
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
@@ -24,11 +42,39 @@ public class SwaggerConfig {
                 .build();
     }
 
+    private SecurityContext securityContext() {
+        return SecurityContext
+                .builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(
+                new SecurityReference(
+                        "Authorization",
+                        authorizationScopes
+                )
+        );
+    }
+
+
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("TTBKK Spring Boot REST API")
                 .version("1.0.0")
                 .description("떡볶이맵 swagger api 입니다.")
                 .build();
+    }
+
+    private ApiKey token() {
+        return new ApiKey(
+                "Authorization",
+                "Authorization",
+                "header"
+                );
     }
 }
