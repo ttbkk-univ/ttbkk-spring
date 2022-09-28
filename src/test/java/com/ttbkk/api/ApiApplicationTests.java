06de56eeeb76e5e998c18bf9438e1773b17c08a8
정지원 <80501465+JIWEON-JEONG@users.noreply.github.com>
2022-09-28 19:23:18 +0900

    테스트용 환경설정 (MYSQL 적용) (#32)
    * [ADD] TEST 전용 yml
    
    * [UPDATE] schema name

--- src/test/java/com/ttbkk/api/ApiApplicationTests.java
+++ src/test/java/com/ttbkk/api/ApiApplicationTests.java
@@ -2,9 +2,7 @@ package com.ttbkk.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootTest
 class ApiApplicationTests {
 
